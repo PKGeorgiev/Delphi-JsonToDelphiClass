629f1a6d03f62812798f44854ed76a6a332e63ae
Jens Borrisholt <GitHub@borrisholt.dk>
2020-07-28 15:35:51 +0200

    Bugfix for string generation

--- Lib/Pkg.Json.Mapper.pas
+++ Lib/Pkg.Json.Mapper.pas
@@ -279,6 +279,8 @@ begin
     else if TRegEx.IsMatch(JsonString.Value,
       '^(-?(?:[1-9][0-9]*)?[0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])T(2[0-3]|[01][0-9]):([0-5][0-9]):([0-5][0-9])(\.[0-9]+)?(Z|[+-](?:2[0-3]|[01][0-9]):[0-5][0-9])?$') then
       Result := jtDateTime
+    else if TryStrToInt64(JsonString.Value, j) then
+      Result := jtString
     else if TryStrToBool(JsonString.Value, b) then
     begin
       if b then
