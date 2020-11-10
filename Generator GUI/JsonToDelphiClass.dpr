9ac38a53c6ff5597c7b5e15fcc0ff5f0200b15ac
Jens Borrisholt <GitHub@borrisholt.dk>
2020-11-10 16:42:22 +0100

    New Feature: PascalCase

--- Generator GUI/JsonToDelphiClass.dpr
+++ Generator GUI/JsonToDelphiClass.dpr
@@ -24,7 +24,9 @@ uses
   DTO.GitHUB.Release in '..\DTO\GitHUB\DTO.GitHUB.Release.pas',
   Pkg.Json.SerializableObject in '..\Lib\Pkg.Json.SerializableObject.pas',
   Pkg.Json.Utils in '..\Lib\Pkg.Json.Utils.pas',
-  Pkg.JSON.SubTypes in '..\Lib\Pkg.JSON.SubTypes.pas';
+  Pkg.JSON.SubTypes in '..\Lib\Pkg.JSON.SubTypes.pas',
+  Pkg.Json.BoundObject in '..\Lib\Pkg.Json.BoundObject.pas',
+  Pkg.Json.Settings in '..\Lib\Pkg.Json.Settings.pas';
 
 {$R *.res}
 
